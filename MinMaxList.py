dd1a73c2cd336b57f9a21e279aa09dd131cc337a
loki00345 <99328513+loki00345@users.noreply.github.com>
2024-02-17 15:07:46 +0200

    Add files via upload

---
+++ MinMaxList.py
@@ -0,0 +1,12 @@
+import random
+
+# a = input("Input: ")
+# print("Your answear is: " + eval(a))
+
+
+
+# list1 = []
+# for i in range(30):
+#     list1.append(random.randint(0, 100))
+#     print(list1[-1])
+# print("Min value:", min(list1))
